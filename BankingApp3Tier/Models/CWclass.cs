906a6b8ab97c7c45b4b2d7cc4f1321045e07231f
sharun kumar <sharunjames12@gmail.com>
2025-09-15 18:57:13 +0530

    add simple class method in the cwclass

---
+++ BankingApp3Tier/Models/CWclass.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingApp3Tier.Models
+{
+    public class CWclass
+    {
+       public void simpelclass()
+        {
+            Console.WriteLine("let see what happened");
+        }
+    }
+}
